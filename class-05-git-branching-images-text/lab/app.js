f70fa4510b59d62387db468cf9a329b8e4f0a846
Shan Kalyanasundaram <shakals@amazon.com>
2019-02-28 23:10:32 -0800

    Completed sumAndMultiply function

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -58,11 +58,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var result = [];
 
+  result[0] = sum(sum(a, b)[0], c)[0];
+  result[1] = multiply(multiply(a, b)[0], c)[0];
+  result[2] = a + ' and ' + b + ' and ' + c + ' sum to ' + result[0] + '.';
+  result[3] = 'The product of ' + a + ' and ' + b + ' and ' + c +  ' is ' + result[1] + '.';
+
+  return result;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
